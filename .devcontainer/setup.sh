5c0fb5454ebe2c9d50fda107c4ae39eac9160bb1
Arash Abadpour <arash@kamangir.net>
2025-01-01 00:51:46 +0000

    boot-up refactors

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Update packages and install dependencies
+apt-get update
+apt-get install -y libgl1-mesa-glx
+
+# Install Python packages
+pip install --upgrade pip
+pip install opencv-python-headless
+pip install abcli blue-geo blueflow
+
+# Append source command to .bashrc
+echo "source $(python3 -m abcli locate)/.abcli/abcli.sh" >> $HOME/.bashrc
