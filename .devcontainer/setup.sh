f0d8172d634ca09ebbe15fdcae879e0acc5abc65
kamangir <arash@kamangir.net>
2024-12-31 18:19:49 -0800

    setup refactor - kamangir/bolt#746

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Update packages and install dependencies
-apt-get update
-apt-get install -y libgl1-mesa-glx
+apt-get update -y
+apt-get install -y libgl1-mesa-glx awscli
 
-# Install Python packages
+# Python packages
 pip install --upgrade pip
 pip install opencv-python-headless
 pip install abcli blue-geo blueflow
 
-# Append source command to .bashrc
-echo "source $(python3 -m abcli locate)/.abcli/abcli.sh" >> $HOME/.bashrc
-
-
+# 🪄 abcli -> .bashrc
+echo "source $(python3 -m abcli locate)/.abcli/abcli.sh" >>$HOME/.bashrc
